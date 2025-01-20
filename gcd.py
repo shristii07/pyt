d5dd6524733976aac22d021612b6480dff3f9f3a
shristii07 <shristipanchal07@gmail.com>
2025-01-20 21:06:53 +0530

    Add files via upload

---
+++ gcd.py
@@ -0,0 +1,7 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> print("gcd of 24,60 is:")
+gcd of 24,60 is:
+>>> print(math.gcd(24,60))
+12
